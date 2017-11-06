60ff6a816d60a2bce4d627e0b2e92d00ad4f0a8a
Lauri Syvänen <lauri.syvanen@courses.tamk.fi>
2017-11-06 14:43:06 +0200

    Player 2:sen controllien alku TEMPeillä

---
+++ Project4/Assets/Scriptit/Player2/Player2Shooting.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Shooting : MonoBehaviour 
+{
+	bool _isFiring;
+	bool bulletsleft;
+	public float firerate = 1f;
+	private float nextfire = 0.0f;
+
+	//public AudioClip Shoot;
+	//private AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		/* _playermove = GetComponent<PlayerController>();
+        if (_playermove != null)
+        { Debug.Log("GUUU	UUUUUF!"); }
+
+		_playermove = GetComponentInParent<PlayerController>();
+		source = GetComponent<AudioSource>();
+		*/
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetAxis("Fire1") > 0 && Time.time > nextfire)
+		{
+			Fire();
+			_isFiring = true;
+			nextfire = Time.time + firerate;
+
+		}
+		else
+		{_isFiring = false;}
+
+	}
+
+	void Fire()
+	{
+		GameObject obj = PoolManager.current.GetBullet();
+		if (obj == null) { bulletsleft = false; return; }
+		else { bulletsleft = true; }
+		obj.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+		obj.transform.rotation = transform.rotation;
+		obj.SetActive(true);
+		//source.PlayOneShot(Shoot);
+	}
+
+
+}
+
