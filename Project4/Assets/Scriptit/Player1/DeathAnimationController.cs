cc01566f1bf39b328230582aeeee16cc1fafee83
Mutti88 <juomakuski@hotmail.com>
2017-11-09 13:51:59 +0200

    DeathAnimation improved

--- Project4/Assets/Scriptit/Player1/DeathAnimationController.cs
+++ Project4/Assets/Scriptit/Player1/DeathAnimationController.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DeathAnimationController : MonoBehaviour {
-
+    public float DeathTimer = 0.4f;
 	// Use this for initialization
 	void Start () {
-		
+        Invoke("DestroySelf",DeathTimer);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+    void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
 }
